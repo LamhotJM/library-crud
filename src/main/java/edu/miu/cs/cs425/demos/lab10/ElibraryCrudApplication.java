12895765ac9dbf65bc2b229eeeab24b9ca925494
Lamhotjm <lamhotjms@gmail.com>
2020-03-10 12:19:49 -0500

    add somting

--- src/main/java/edu/miu/cs/cs425/demos/lab10/ElibraryCrudApplication.java
+++ src/main/java/edu/miu/cs/cs425/demos/lab10/ElibraryCrudApplication.java
@@ -2,12 +2,19 @@ package edu.miu.cs.cs425.demos.lab10;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ElibraryCrudApplication {
+public class ElibraryCrudApplication  extends SpringBootServletInitializer {
 
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ElibraryCrudApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ElibraryCrudApplication.class, args);
 	}
-
 }
